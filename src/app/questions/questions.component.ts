fb137f4e5fe70b79febb54c2dc8c707238696602
Benjamim Chagan <benjamimbchagan@gmail.com>
2019-12-18 11:39:10 +0000

    Add question service and related method

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../core/services';
 import { FormGroup, FormControl } from '@angular/forms';
+import { QuestionService } from '../core/services/question.service';
 
 @Component({
   selector: 'app-questions',
@@ -18,9 +19,27 @@ export class QuestionsComponent implements OnInit {
       msg: "qwerty  zxcvb"
     }
   ]
+  // criar o objeto para por no ng model
+  //criaçao  da variavel
+    question = {
+      questionContent:'',
+      conference : {id:1}
+    };
 
-  
+  constructor(private questionService:QuestionService){}
+
   ngOnInit() {
+    this.questionService.getAll().subscribe( data => {
+      console.log(data);
+      
+    })
+  }
+// esta funçãoo retorna o pedido
+  submitQuestion(){
+    this.questionService.addQuestion(this.question).subscribe( data => {
+      console.log(data);
+      
+    })
   }
 
 }
